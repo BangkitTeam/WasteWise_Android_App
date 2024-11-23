ac69600c712ebd25f7797c499d41c978f4efc21b
haikal <asmanhaikal@gmail.com>
2024-11-23 22:18:15 +0700

    fix: add intent logic to welcome screen

--- app/src/main/java/com/team/wastewise/view/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/team/wastewise/view/welcome/WelcomeActivity.kt
@@ -1,11 +1,14 @@
 package com.team.wastewise.view.welcome
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.team.wastewise.R
+import com.team.wastewise.view.login.LoginActivity
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,12 @@ class WelcomeActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnStartHere: Button = findViewById(R.id.btn_start_here)
+        btnStartHere.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
