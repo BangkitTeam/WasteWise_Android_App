33a623db5b9e56a3d3a4f388dd2eb0acf90935cc
kukuhks <kukuhh.ks@gmail.com>
2024-11-25 01:33:36 +0700

    Remove Bottom nav view from upload fragment

--- app/src/main/java/com/team/wastewise/view/upload/UploadFragment.kt
+++ app/src/main/java/com/team/wastewise/view/upload/UploadFragment.kt
@@ -6,26 +6,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.team.wastewise.R
+import com.team.wastewise.databinding.FragmentHomeBinding
+import com.team.wastewise.databinding.FragmentUploadBinding
 
 class UploadFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = UploadFragment()
-    }
+    private var _binding: FragmentUploadBinding? = null
+    private val binding get() = _binding!!
 
     private val viewModel: UploadViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        // TODO: Use the ViewModel
+        val bottomNavigationView = requireActivity().findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNavigationView.visibility = View.GONE
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_upload, container, false)
+        _binding = FragmentUploadBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        val bottomNavigationView = requireActivity().findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNavigationView.visibility = View.VISIBLE
+        _binding = null
     }
 }
