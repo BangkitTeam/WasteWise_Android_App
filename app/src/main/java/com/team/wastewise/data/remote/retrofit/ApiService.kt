1f21dc727c1dff7b1f6f4dbea7b30d05a97d9f73
haikal <asmanhaikal@gmail.com>
2024-12-05 02:14:31 +0700

    feat(register): adding register feature logic

--- app/src/main/java/com/team/wastewise/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/team/wastewise/data/remote/retrofit/ApiService.kt
@@ -1,8 +1,11 @@
 package com.team.wastewise.data.remote.retrofit
 
 import com.team.wastewise.data.remote.response.FileUploadResponse
+import com.team.wastewise.data.remote.response.LoginResponse
+import com.team.wastewise.data.remote.response.RegisterResponse
 import okhttp3.MultipartBody
-import retrofit2.http.Header
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
@@ -14,4 +17,21 @@ interface ApiService {
 //        @Header("Authorization") token: String,
         @Part file: MultipartBody.Part
     ) : FileUploadResponse
+
+    // Endpoint to register a new user
+    @FormUrlEncoded
+    @POST("auth/register")
+    suspend fun register(
+        @Field("username") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): RegisterResponse
+
+    // Endpoint to log in an existing user
+    @FormUrlEncoded
+    @POST("login")
+    suspend fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): LoginResponse
 }
