843d3d394bb472486de29c8d17dc3123e02fa54b
haikal <asmanhaikal@gmail.com>
2024-12-06 14:01:40 +0700

    feat(policy): adding policy page logic

--- app/src/main/java/com/team/wastewise/view/profile/PolicyActivity.kt
+++ app/src/main/java/com/team/wastewise/view/profile/PolicyActivity.kt
@@ -1,21 +1,46 @@
 package com.team.wastewise.view.profile
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.team.wastewise.R
+import com.team.wastewise.databinding.ActivityAboutBinding
+import com.team.wastewise.databinding.ActivityPolicyBinding
 
 class PolicyActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPolicyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_policy)
+        binding = ActivityPolicyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Set up the toolbar
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = getString(R.string.privacy_policy_title)
+    }
+
+
+    // Override onOptionsItemSelected to handle the back button press
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                // Navigate back
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
