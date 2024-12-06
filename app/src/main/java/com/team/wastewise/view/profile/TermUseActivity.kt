d5231833425c2941bee261e90bde407734293914
haikal <asmanhaikal@gmail.com>
2024-12-06 11:31:49 +0700

    feat(profile): adding static pages, like setting, about, and so on

---
+++ app/src/main/java/com/team/wastewise/view/profile/TermUseActivity.kt
@@ -0,0 +1,21 @@
+package com.team.wastewise.view.profile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.team.wastewise.R
+
+class TermUseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_term_use)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
